501920796d6125457fd9c44467e29c376822aaab
Eugenia-d <e.drobiazko@gmail.com>
2021-10-21 21:57:32 +0300

    started working on task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
+// Change code below this line
 console.log(galleryItems);
+
+const galleryDivEl = document.getElementById("gallery");
+
+// 1 добавить (используя map) картинки из galleryItems в галерею galleryDivEl
+const elements = galleryItems.map((item) => {
+  return element; // TODO
+});
+galleryDivEl.append(...elements);
+
+// 2 на каждую картинку в галерее добавить слушатель нажатия
+
+// 3 при нажатии открывать модальное окно
+
+openModal(galleryItems[0].original);
+
+function openModal(url) {
+  const instance = basicLightbox.create(`
+    <div class="gallery__link">
+        <img src=${url} alt=${galleryItems[0].description}/>
+    </div>
+`);
+
+  instance.show();
+}
